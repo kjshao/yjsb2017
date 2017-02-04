d34fb7dc18782e0f28f6382ce655e5cb0ba841d1
kjshao <865694264@qq.com>
2017-02-05 00:39:52 +0800

    new

--- yjsb2017/global.php
+++ yjsb2017/global.php
@@ -1,6 +1,8 @@
 <?php
   define ('__ROOT__', dirname(__FILE__));
-  $tmp1 = explode("/yjsb2017", __ROOT__)[0]; // /var/www/html/aa/bb/..
-  $tmp2 = explode($_SERVER['DOCUMENT_ROOT'],$tmp1)[1]; // /aa/bb/..
+  $tmp = explode("/yjsb2017", __ROOT__); // /var/www/html/aa/bb/..
+  $tmp1 = $tmp[0];
+  $tmp = explode($_SERVER['DOCUMENT_ROOT'],$tmp1); // /aa/bb/..
+  $tmp2 = $tmp[1];
   define ('__SERVER__', $tmp2."/yjsb2017");
 ?>
